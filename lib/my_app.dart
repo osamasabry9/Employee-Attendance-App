a2fde22d5075d2b41dc7d35d4d253321a79ebdd9
osamasabry9 <osamasabry324@gmail.com>
2024-12-09 00:27:07 +0400

    Add routing functionality and update main app structure
    This commit introduces a new routing system using GoRouter
    and defines application routes in separate files. The
    `AppRoutes` class manages route generation, while the
    `AppRouter` class sets up the GoRouter with initial routes
    and error handling. Additionally, the main application
    structure in `my_app.dart` is updated to utilize
    `ScreenUtil` for responsive design and to integrate the
    new router configuration.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,20 +1,32 @@
-import 'package:employee_attendance/shared/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/routing/app_router.dart';
+import 'core/constants/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Attendance',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Employee Attendance',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.darkTheme,
+          routerConfig: AppRouter.router,
+          builder: (context, child) {
+            // Apply screen util to text scaling
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: child!,
+            );
+          },
+        );
+      },
     );
   }
 }
