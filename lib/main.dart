500a87946c8450040f6f04ee63fba7a5bca7cc2a
osamasabry9 <osamasabry324@gmail.com>
2024-12-09 06:17:57 +0400

    Fix screen size issue in release mode
    Ensure the screen size is properly initialized by calling
    ScreenUtil.ensureScreenSize() in the main function. This
    addresses the bug where texts were hidden when using
    flutter_screenutil in release mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/di/injection_container.dart';
 import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
 
   // Set preferred orientations
   await SystemChrome.setPreferredOrientations([
